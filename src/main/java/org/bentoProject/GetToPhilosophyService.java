04637c789f96fc136d872f220ad8a3152bd7413f
Farhan Khan <fkhan821@gmail.com>
2017-10-17 14:23:27 -0500

    Initial commit with project structure and pom.xml

---
+++ src/main/java/org/bentoProject/GetToPhilosophyService.java
@@ -0,0 +1,26 @@
+package org.bentoProject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.sql2o.Sql2o;
+import org.sql2o.converters.UUIDConverter;
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
+import static spark.Spark.delete;
+import static spark.Spark.port;
+
+/**
+ *
+ * @author Farhan Khan
+ */
+public class GetToPhilosophyService {
+    
+    public static void main( String[] args) {
+    
+    }
+}
