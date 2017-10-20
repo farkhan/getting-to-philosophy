8053c418ee741b4f9c0f801850b1056653249287
Farhan Khan <fkhan821@gmail.com>
2017-10-20 14:39:08 -0500

    Update DB schema

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -1,30 +1,35 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  * Author:  Farhan Khan
  * Created: Oct 17, 2017
  */
 
-CREATE TABLE `bento`.`url` (
-  `url_id` INT NOT NULL AUTO_INCREMENT,
-  `data` VARCHAR(255) NULL,
-  `title` VARCHAR(100) NULL,
-  `urlcol` VARCHAR(45) NULL,
-  PRIMARY KEY (`url_id`));
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE `bento`.`path` (
-  `path_id` INT NOT NULL AUTO_INCREMENT,
-  `url_id` INT NULL,
-  `data` VARCHAR(255) NULL,
-  `hop` INT NULL,
-  `title` VARCHAR(100) NULL,
-  PRIMARY KEY (`path_id`),
-  INDEX `url_id_idx` (`url_id` ASC),
-  CONSTRAINT `url_id`
-    FOREIGN KEY (`url_id`)
-    REFERENCES `bento`.`url` (`url_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+-- ----------------------------
+-- Table structure for path
+-- ----------------------------
+DROP TABLE IF EXISTS `path`;
+CREATE TABLE `path`  (
+  `path_id` int(11) NOT NULL AUTO_INCREMENT,
+  `url_id` int(11) NULL DEFAULT NULL,
+  `data` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `hop` int(11) NULL DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`path_id`) USING BTREE,
+  INDEX `url_id_idx`(`url_id`) USING BTREE,
+  CONSTRAINT `url_id` FOREIGN KEY (`url_id`) REFERENCES `url` (`url_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE = InnoDB AUTO_INCREMENT = 173 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for url
+-- ----------------------------
+DROP TABLE IF EXISTS `url`;
+CREATE TABLE `url`  (
+  `url_id` int(11) NOT NULL AUTO_INCREMENT,
+  `data` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `title` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`url_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 21 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
