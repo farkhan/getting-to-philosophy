7a5080d5b7ef7318b6a814647decf2d0e542606c
Farhan Khan <fkhan821@gmail.com>
2017-10-20 10:38:03 -0500

    Show error msg on empty string, show hide spinner

--- src/main/resources/public/js/form.js
+++ src/main/resources/public/js/form.js
@@ -1,31 +1,29 @@
 $(function() {
     $(document).ready(function(){
-        // click on button submit
         $('span.glyphicon-refresh').hide();
         $("#submit").on('click', function(){
-            $('.glyphicon').removeClass('glyphicon-search');
-            $('.glyphicon').addClass('spinner');
             var urlInput = $("#url").val();
-            $.ajax({
-                url: 'url',
-                type : "POST",
-                data: JSON.stringify({url: urlInput}),
-                contentType: "application/json; charset=utf-8",
-                success : function(result) {
-                    $("#pathList").html(
-                        $("#pathTemplate").render(result, true)
-                    );
-                    console.log(result);
-                },
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                },
-                always: function() {
-                    console.log('always');
-                    $('.glyphicon').removeClass('spinner');
-                    $('.glyphicon').addClass('glyphicon-search');
-                }
-            });
+            if (isFormValid()) {
+                $('.glyphicon').removeClass('glyphicon-search');
+                $('.glyphicon').addClass('spinner');
+                $.ajax({
+                    url: 'url',
+                    type : "POST",
+                    data: JSON.stringify({url: urlInput}),
+                    contentType: "application/json; charset=utf-8",
+                    success : function(result) {
+                        $("#pathList").html(
+                            $("#pathTemplate").render(result, true)
+                        );
+                        enableSearchButton()
+                        console.log(result);
+                    },
+                    error: function(xhr, resp, text) {
+                        enableSearchButton()
+                        console.log(xhr, resp, text);
+                    }
+                });
+            }
         });
         $('#url').keypress(function(e) {
             if(e.which === 13) {
@@ -34,5 +32,20 @@ $(function() {
                 $('#submit').focus().click();
             }
         });
+        function isFormValid() {
+            var success = true;
+            var msg = "";
+            if ($("#url").val() == "") {
+                msg = "Please enter a URL";
+                success = false;
+            }
+            $("#errorMsg").html(msg)
+            return success;
+        }
+
+        function enableSearchButton() {
+            $('.glyphicon').removeClass('spinner');
+            $('.glyphicon').addClass('glyphicon-search');
+        }
     });
 });
